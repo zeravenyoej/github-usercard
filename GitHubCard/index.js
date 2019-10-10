1dbffae5be9eb9877effb7c16f66b4a9071ac15e
Joey Nevarez <josephnevarez12@gmail.com>
2019-10-09 19:38:14 -0700

    set up axios

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,14 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/zeravenyoej')
+  .then(res=>{
+    console.log(res);
+  })
+  .catch(err=>{
+    console.log(err);
+  });
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
